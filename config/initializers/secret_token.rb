fa58693a7113e70f47e9a0539d9cbfe02d3705e2
Evan Potter <epotter12@gmail.com>
2013-12-10 01:52:26 +0000

    added rails site and bootstrap navbar

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Shadetree::Application.config.secret_key_base = '5c3227f5aab35a649c552cfe62dca07d57dce8c58ca3bbc6c0ee4d5c779c390b204f418260252e9ffc95894f250881b32f99db7dff0856a99c303c4ffc9fa99c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Shadetree::Application.config.session_store :cookie_store, key: '_shadetree_session'
